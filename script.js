8a30b869978b528083620d53477e6559e69d2cd1
mattbrettell <MattBrettell@Traceys-iMac.local>
2017-05-02 19:20:50 +0100

    contact button

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 window.onload = function() {
   document.getElementById("pagethree").style.display = "none";
-    document.getElementById("pagetwo").style.display = "none";
+    document.getElementById("contact").style.display = "none";
 };
+
+function contact() {
+    document.getElementById("pageone").style.display = "none";
+    document.getElementById("contact").style.display = "block";
+    
+}
